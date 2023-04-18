4856119e777d0aa6e27efd5c1c6913b3f354f080
rirefat <rirefat.official@gmail.com>
2023-04-18 18:02:21 +0600

    update visitors

--- src/FolderStructure/Components/Visitors/Visitors.js
+++ src/FolderStructure/Components/Visitors/Visitors.js
@@ -20,21 +20,21 @@ const Visitors = () => {
                         <tr>
                             <th>1</th>
                             <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
+                            <td>Brother</td>
                             <th>12 June 2022</th>
                         </tr>
                         {/* row 2 */}
                         <tr>
                             <th>2</th>
                             <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
+                            <td>Cousin</td>
                             <th>12 June 2022</th>
                         </tr>
                         {/* row 3 */}
                         <tr>
                             <th>3</th>
                             <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
+                            <td>Uncle</td>
                             <th>12 June 2022</th>
                         </tr>
                     </tbody>
